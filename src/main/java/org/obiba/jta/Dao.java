391f8329ce7ef4cb081ea091950df0b24e4338c7
Cedric Thiebault <cthiebault@p3g.org>
2013-11-05 16:34:27 -0500

    Setup test

--- src/main/java/org/obiba/jta/Dao.java
+++ src/main/java/org/obiba/jta/Dao.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Component
 public class Dao {
+
+  @Autowired
+  private TransactionTemplate transactionTemplate;
 
   public void createEntity(String name, SessionFactory sessionFactory) {
     Session currentSession = sessionFactory.getCurrentSession();
--- src/test/java/org/obiba/jta/JtaTest.java
+++ src/test/java/org/obiba/jta/JtaTest.java
@@ -1,20 +1,11 @@
 package org.obiba.jta;
 
-import java.beans.PropertyVetoException;
-import java.util.List;
-
-import javax.sql.DataSource;
-
-import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @ContextConfiguration("classpath:application-context.xml")
 public class JtaTest extends AbstractJUnit4SpringContextTests {
@@ -22,33 +13,14 @@ public class JtaTest extends AbstractJUnit4SpringContextTests {
   private static final Logger log = LoggerFactory.getLogger(JtaTest.class);
 
   @Autowired
-  private DataSourceFactory dataSourceFactory;
-
-  @Autowired
-  private SessionFactoryFactory sessionFactoryFactory;
-
-  @Autowired
-  private Dao dao;
-
-  @Autowired
-  private TransactionTemplate transactionTemplate;
+  private IntegrationService integrationService;
 
   @Test
-  public void test() throws PropertyVetoException {
+  public void test() throws Exception {
 
-    DataSource dataSource = dataSourceFactory.createDataSource("db1");
-    final SessionFactory sessionFactory = sessionFactoryFactory.createSessionFactory(dataSource);
-
-    transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-      @Override
-      protected void doInTransactionWithoutResult(TransactionStatus status) {
-        dao.createEntity("cedric", sessionFactory);
-        List<Entity> entities = dao.listEntities(sessionFactory);
-        log.info("entities: {}", entities);
-      }
-    });
-
-    sessionFactory.close();
+    for(int i = 0; i < 5; i++) {
+      integrationService.createEntities("database-" + i);
+    }
 
   }
 
