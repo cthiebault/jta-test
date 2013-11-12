d4bccab99d9a37f63bf37a72e0b422bf4421a46c
Cedric Thiebault <cthiebault@p3g.org>
2013-11-12 11:18:13 -0500

    Replace RestEasy by Jersey

--- src/main/java/org/obiba/jta/Run.java
+++ src/main/java/org/obiba/jta/Run.java
@@ -1,6 +1,7 @@
 package org.obiba.jta;
 
 import org.obiba.jta.web.cfg.JettyServer;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.support.GenericApplicationContext;
 
@@ -9,6 +10,12 @@ public class Run {
   private Run() {}
 
   public static void main(String... args) {
+
+    //  remove existing handlers attached to java.util.logging root logger
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    // add SLF4JBridgeHandler to java.util.logging's root logger
+    SLF4JBridgeHandler.install();
+
     GenericApplicationContext context = new GenericApplicationContext();
     XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(context);
     xmlReader.loadBeanDefinitions("classpath:application-context.xml");
--- src/main/java/org/obiba/jta/web/TestResource.java
+++ src/main/java/org/obiba/jta/web/TestResource.java
@@ -1,8 +1,12 @@
 package org.obiba.jta.web;
 
+import javax.annotation.Resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import org.obiba.jta.service.IntegrationService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
@@ -12,9 +16,20 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 @Path("/test")
 public class TestResource {
 
+  @Resource
+  private IntegrationService integrationService;
+
+  @GET
+  @Path("/inject")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String testInjection() {
+    return "integrationService: " + integrationService;
+  }
+
   @GET
   @Path("/active")
-  public String isTransactionActive() {
+  @Produces(MediaType.TEXT_PLAIN)
+  public String testActiveTransaction() {
     return "isActualTransactionActive: " + TransactionSynchronizationManager.isActualTransactionActive();
   }
 
